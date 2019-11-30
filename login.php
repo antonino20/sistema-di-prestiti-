ee9144f9af2215059777f0c1d3d2d951933f8931
steve <steven.97@tiscali.it>
2019-11-30 12:11:17 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,35 +1,38 @@
 <head>
-<link href="stilo.css" type="text/css" rel="stylesheet">
+<link href="conf.css" type="text/css" rel="stylesheet">
 <meta name="viewport" content="width=device-width">
 
 <?php
-
-$conex = mysql_connect("localhost", "wardrivers");
+$user = "";
+	
+$conex = mysql_connect("localhost", "$user");
 mysql_select_db("my_wardrivers", $conex);
 
 
 $email = $_POST['email'];
 $pw = $_POST['password'];
 
+$table; 
 
-$sql = "SELECT email, password FROM utenti WHERE email='$email' AND password='$pw'";
-$controllo = mysql_query($sql) or die (mysql_error());
-print_r($controllo)."<br>";
+
+$sql = "SELECT email, password FROM $table WHERE email='$email' AND password='$pw'";
+$check = mysql_query($sql) or die (mysql_error());
+print_r($check)."<br>";
 echo "ok";
 
-if(mysql_num_rows($controllo) == 1) { 
-echo "Benvenuto";
+if(mysql_num_rows($check) == 1) { 
+echo "welcome";
 ?>
 <div id="form">
 	<form method="get" action="database.php">
     <table>
     
     	<tr>
-        	<td>artista preferito</td>
-        	<td><input type="text" name="nome"></td>
+        	<td>favorite artist</td>
+        	<td><input type="text" name="name"></td>
         </tr>
 
-        <td><input type="submit" value="invio"></td>
+        <td><input type="submit"></td>
      </table>
     </form>
 
@@ -44,4 +47,4 @@ echo "Benvenuto";
 else {header('location:../');}
 
 
-?>
+?>
