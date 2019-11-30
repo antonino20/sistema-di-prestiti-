53c830224cf50dd079aeac96714be5a384fa81ad
steve <steven.97@tiscali.it>
2019-11-30 11:03:14 +0100

    Update conferma.php

--- conferma.php
+++ conferma.php
@@ -1,25 +1,22 @@
 <?php
-/* PARAMETRI DI CONESSIONE AL MIO DB MYSQL */
 $conex = mysql_connect("localhost", "wardrivers");
 mysql_select_db("my_wardrivers", $conex);
 
-/* il token e il parametro ricevuto dall url inviato tramite posta */
 
 $token = $_GET['token'];
 
 
-$sql = "SELECT token FROM utenti WHERE token='$token'";
+$sql = "SELECT token FROM users WHERE token='$token'";
 
-$verifica = mysql_query($sql);
+$verify = mysql_query($sql);
 
 
-if(mysql_num_rows($verifica) == 1) {
-	echo "ok";
+if(mysql_num_rows($verify) == 1) {
 	$query = "UPDATE utenti set attivo='1' WHERE token='$token'";
 	mysql_query($query) or die (mysql_error());
-	echo "Account attivato";
+	echo "Account actived";
 }
-else {echo "nn ci sono risultati nel db";}
+else {echo "err";}
 
 
-?>
+?>
